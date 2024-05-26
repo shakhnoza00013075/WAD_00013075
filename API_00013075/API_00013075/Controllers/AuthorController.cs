251fc97e82d297246f2e81f914ea4977247553b4
00013075 <113843921+shakhnoza00013075@users.noreply.github.com>
2024-05-26 16:40:13 +0500

    controllers

---
+++ API_00013075/API_00013075/Controllers/AuthorController.cs
@@ -0,0 +1,54 @@
+﻿using DAL_00013075.Models_00013075;
+using DAL_00013075.Repositories_00013075;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_00013075.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IRepository<Author> repository;
+
+        public AuthorController(IRepository<Author> repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        { 
+            return Ok(await repository.GetAll());
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var author = await repository.Get(id);
+            if(author == null) return NotFound();
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Author author)
+        {
+            await repository.Create(author);
+            return Ok();
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> Edit(int id, Author author)
+        {
+            await repository.Edit(id, author);
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ API_00013075/API_00013075/Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using DAL_00013075.Models_00013075;
+using DAL_00013075.Repositories_00013075;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_00013075.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IRepository<Book> repository;
+
+        public BookController(IRepository<Book> repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await repository.GetAll());
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var book = await repository.Get(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Book book)
+        {
+            await repository.Create(book);
+            return Ok();
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> Edit(int id, Book book)
+        {
+            await repository.Edit(id, book);
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await repository.Delete(id);
+            return Ok();
+        }
+    }
+}
